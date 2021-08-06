59b7808ad2550e5e87c01d0c23a67978ac69a500
Kwitsch <kwitsch@gmial.com>
2021-08-06 18:32:29 +0200

    added invalid ip check

--- src/sc/sc.go
+++ src/sc/sc.go
@@ -30,7 +30,9 @@ func New(name, hostname string, verbose bool) Container {
 
 func (c *Container) Lookup() {
 	ips, err := net.LookupIP(c.Hostname)
-	if err != nil {
+	if err != nil ||
+		len(ips) == 0 ||
+		(len(ips) == 1 && ips[0].String() == "0.0.0.0") {
 		c.Active = false
 		c.Addresses = []net.IP{}
 	} else {
